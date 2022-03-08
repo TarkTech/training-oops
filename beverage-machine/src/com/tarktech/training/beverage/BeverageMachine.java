cf195ed8821dda90321cdfe9b819c4fb627b5f68
Vimal Parakhiya <vimal.parakhiya@tarktech.com>
2022-03-09 03:06:20 +0530

    Added Cappuccino and Mocha as additional beverages

--- beverage-machine/src/com/tarktech/training/beverage/BeverageMachine.java
+++ beverage-machine/src/com/tarktech/training/beverage/BeverageMachine.java
@@ -3,16 +3,25 @@ package com.tarktech.training.beverage;
 public class BeverageMachine {
     private int availableLatteCount;
     private int availableTeaCount;
+    private int availableCappuccinoCount;
+    private int availableMochaCount;
 
     private int totalLatteDispensed;
     private int totalTeaDispensed;
+    private int totalCappuccinoDispensed;
+    private int totalMochaDispensed;
 
     public BeverageMachine() {
         availableLatteCount = 5;
         availableTeaCount = 5;
+        availableCappuccinoCount=5;
+        availableMochaCount=5;
 
         totalLatteDispensed = 0;
         totalTeaDispensed = 0;
+        totalCappuccinoDispensed=0;
+        totalMochaDispensed=0;
+
     }
 
     public String makeBeverage(BeverageType beverageType) {
@@ -31,6 +40,20 @@ public class BeverageMachine {
                 totalTeaDispensed++;
                 availableTeaCount--;
                 return "Enjoy your hot Masala Tea!";
+            case Cappuccino:
+                if(availableCappuccinoCount == 0){
+                    throw new RuntimeException("Sorry! Cappuccino is out of stock!");
+                }
+                totalCappuccinoDispensed++;
+                availableCappuccinoCount--;
+                return "Enjoy freshly brewed Cappuccino!";
+            case Mocha:
+                if(availableMochaCount == 0){
+                    throw new RuntimeException("Sorry! Mocha is out of stock!");
+                }
+                totalMochaDispensed++;
+                availableMochaCount--;
+                return "Enjoy Swiss Dark Chocolate Mocha!";
             default:
                 throw new RuntimeException("Unsupported beverage: " + beverageType);
         }
@@ -42,6 +65,10 @@ public class BeverageMachine {
                 return totalLatteDispensed;
             case Tea:
                 return totalTeaDispensed;
+            case Cappuccino:
+                return totalCappuccinoDispensed;
+            case Mocha:
+                return totalMochaDispensed;
         }
         return 0;
     }
@@ -52,15 +79,19 @@ public class BeverageMachine {
                 return availableLatteCount;
             case Tea:
                 return availableTeaCount;
+            case Cappuccino:
+                return availableCappuccinoCount;
+            case Mocha:
+                return availableMochaCount;
         }
         return 0;
     }
 
     public int getTotalBeveragesDrunkByUser(){
-        return totalLatteDispensed + totalTeaDispensed;
+        return totalLatteDispensed + totalTeaDispensed + totalCappuccinoDispensed + totalMochaDispensed;
     }
 
     public int getTotalAvailableBeverages(){
-        return availableLatteCount + availableTeaCount;
+        return availableLatteCount + availableTeaCount + availableCappuccinoCount + availableMochaCount;
     }
 }
