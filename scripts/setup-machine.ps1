cf7cd7dea4c41f98d5a3bedff1a9919f35eb34ef
Rumit Parakhiya <2221855+rparakhiya@users.noreply.github.com>
2022-03-22 16:23:27 +0530

    Gain elevated privileges

--- scripts/setup-machine.ps1
+++ scripts/setup-machine.ps1
@@ -1,3 +1,24 @@
+if (!
+    #current role
+    (New-Object Security.Principal.WindowsPrincipal(
+        [Security.Principal.WindowsIdentity]::GetCurrent()
+    #is admin?
+    )).IsInRole(
+        [Security.Principal.WindowsBuiltInRole]::Administrator
+    )
+) {
+    #elevate script and exit current non-elevated runtime
+    Start-Process `
+        -FilePath 'powershell' `
+        -ArgumentList (
+            #flatten to single array
+            '-File', $MyInvocation.MyCommand.Source, $args `
+            | %{ $_ }
+        ) `
+        -Verb RunAs
+    exit
+}
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 cinst -y git openjdk intellijidea-community
@@ -5,3 +26,7 @@ cinst -y git openjdk intellijidea-community
 mkdir D:\tark-training
 Set-Location D:\tark-training
 git clone https://github.com/TarkTech/training-oops.git
+
+Write-Output "🎉 Setup completed."
+
+Pause
